f6c96f587e65b3102af3492e2076221165e45221
show108 <s.segunshowunmi@gmail.com>
2023-10-10 17:23:24 +0100

    working on the institution page

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,76 @@
-import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
-import './App.css';
-import {Home} from './pages/Home';
-import About from './pages/About';
-import Price from './pages/Price';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import RootLayouts from './layouts/RootLayouts';
-import NotFound from './pages/NotFound';
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/Home";
+import About from "./pages/About";
+import Price from "./pages/Price";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import RootLayouts from "./layouts/RootLayouts";
+import NotFound from "./pages/NotFound";
 
-import Student, { studentRegister } from './pages/signin/Student';
-import Lecturer, { lecturerRegister } from './pages/signin/Lecturer';
-import Register_Institution, { institutionRegister } from './pages/Register_Institution';
-import InstitutionMain from './pages/institution/InstitutionMain'
-import StudentLayout from './layouts/StudentLayout';
-import RegisterLayout from './layouts/RegisterLayout';
-import Signin from './pages/Signin';
-import LecturerLayout from './layouts/LecturerLayout';
-import LecturerMain from './pages/lecturer/LecturerMain';
-import StudentMain from './pages/student/StudentMain';
-import LecturerHome from './pages/lecturer/LecturerHome';
-import InstitutionLayout from './layouts/InstitutionLayout';
+import Student, { studentRegister } from "./pages/signin/Student";
+import Lecturer, { lecturerRegister } from "./pages/signin/Lecturer";
+import Register_Institution, {
+  institutionRegister,
+} from "./pages/Register_Institution";
+import InstitutionMain from "./pages/institution/InstitutionMain";
+import StudentLayout from "./layouts/StudentLayout";
+import RegisterLayout from "./layouts/RegisterLayout";
+import Signin from "./pages/Signin";
+import LecturerLayout from "./layouts/LecturerLayout";
+import LecturerMain from "./pages/lecturer/LecturerMain";
+import StudentMain from "./pages/student/StudentMain";
+import LecturerHome from "./pages/lecturer/LecturerHome";
+import InstitutionLayout from "./layouts/InstitutionLayout";
+import Construction from "./pages/Construction";
+import InstitutionHome from "./pages/institution/InstitutionHome";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-        <Route path='/' element={<RootLayouts />}>
-          <Route index element={<Home />}/>
-          <Route path='about' element={<About />}/>
-          <Route path='price' element={<Price />}/>
-          <Route path='contact' element={<Contact />}/>
-          <Route path='login' element={<Login />}/>
-          <Route path='register-institution' element={<Register_Institution/>} action={institutionRegister} />         
-          <Route path='signin' element={<RegisterLayout />}>
-            <Route index element={<Signin/>}/>
-            <Route path='student' element={<Student/>} action={studentRegister}/>
-            <Route path='lecturer' element={<Lecturer/>} action={lecturerRegister}/>
-          </Route>
-          <Route path='lecturer' element={<LecturerLayout/>}>
-              <Route index element={<LecturerMain/>} />
-              <Route path='home' element={<LecturerHome/>}/>
-          </Route>
-          <Route path='student' element={<StudentLayout/>}>
-              <Route index element={<StudentMain/>} />
-          </Route>
-          <Route path='institution' element={<InstitutionLayout/>}>
-              <Route index element={<InstitutionMain/>}/>
-          </Route>
-          <Route path='*' element={<NotFound />} />
-
-        </Route>
-        
+    <Route path="/" element={<RootLayouts />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="price" element={<Price />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="login" element={<Login />} />
+      <Route
+        path="register-institution"
+        element={<Register_Institution />}
+        action={institutionRegister}
+      />
+      <Route path="signin" element={<RegisterLayout />}>
+        <Route index element={<Signin />} />
+        <Route path="student" element={<Student />} action={studentRegister} />
+        <Route
+          path="lecturer"
+          element={<Lecturer />}
+          action={lecturerRegister}
+        />
+      </Route>
+      <Route path="lecturer" element={<LecturerLayout />}>
+        <Route index element={<LecturerMain />} />
+        <Route path="home" element={<LecturerHome />} />
+      </Route>
+      <Route path="student" element={<StudentLayout />}>
+        <Route index element={<StudentMain />} />
+      </Route>
+      <Route path="institution" element={<InstitutionLayout />}>
+        <Route index element={<InstitutionMain />} />
+        <Route path="home" element={<InstitutionHome />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+      <Route path="construction" element={<Construction />} />
+    </Route>
   )
-)
+);
 
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/layouts/InstitutionLayout.jsx
+++ src/layouts/InstitutionLayout.jsx
@@ -1,95 +1,136 @@
-import {  Avatar, Box, Button, Flex, Grid, GridItem, IconButton, List, ListIcon, ListItem, Menu, MenuButton, MenuItem, MenuList, Spacer, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
-import { Link, Outlet } from "react-router-dom";
-import { MdAnnouncement, MdBook, MdHelp,MdSettingsSuggest, MdSsidChart, MdVisibility } from 'react-icons/md'
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  IconButton,
+  List,
+  ListIcon,
+  ListItem,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+  Stack,
+  Text,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import {
+  MdAnnouncement,
+  MdBook,
+  MdHelp,
+  MdSettingsSuggest,
+  MdSsidChart,
+  MdVisibility,
+} from "react-icons/md";
 import { useEffect } from "react";
-import logo from '../images/logo.png'
+import logo from "../images/logo.png";
 import { AddIcon, SearchIcon } from "@chakra-ui/icons";
 export default function InstitutionLayout() {
-    useEffect(() =>{
-        document.body.classList.add('bg-color')
-    }, [])
+  useEffect(() => {
+    document.body.classList.add("bg-color");
+  }, []);
 
   return (
     <div>
-        <Grid templateColumns={'repeat(6, 1fr)'}>
-          <GridItem
-          as={'aside'}
-          colSpan={'1'}
-          minHeight={'100vh'}
-          borderRight={'2px solid black'}
-          >
-          <Flex mt={'40px'} ml={'10px'}>
-          <Text fontWeight={'extrabold'} fontSize={'2xl'}>Hello! Segun</Text>
+      <Grid templateColumns={"repeat(6, 1fr)"}>
+        <GridItem
+          as={"aside"}
+          colSpan={"1"}
+          minHeight={"100vh"}
+          borderRight={"2px solid black"}>
+          <Flex mt={"30px"} ml={"10px"}>
+            <Text fontWeight={"extrabold"} fontSize={"2xl"}>
+              University of Ibadan
+            </Text>
           </Flex>
-          <Flex justify={'center'}>
-          <List spacing={10} mt={'70px'} cursor={'pointer'}>
-            <Link to={'home'}>
-              <ListItem fontWeight={'bold'} cursor={'pointer'} pl={'20px'}>
-                <ListIcon as={MdVisibility} boxSize={5}/>
-                Overview
-              </ListItem>
-            </Link>
-                
-           <ListItem fontWeight={'bold'} cursor={'pointer'} pl={'20px'}>
-              <ListIcon as={MdBook} boxSize={5} />
+          <Flex justify={"center"}>
+            <List spacing={10} mt={"70px"} cursor={"pointer"}>
+              <NavLink
+                to={"home"}
+                style={({ isActive }) => {
+                  return {
+                    color: isActive ? "Red" : "black",
+                  };
+                }}>
+                <ListItem fontWeight={"bold"} cursor={"pointer"} pl={"20px"}>
+                  <ListIcon as={MdVisibility} boxSize={5} />
+                  Overview
+                </ListItem>
+              </NavLink>
+              <ListItem fontWeight={"bold"} cursor={"pointer"} pl={"20px"}>
+                <ListIcon as={MdBook} boxSize={5} />
                 Courses
-              </ListItem> 
-              <ListItem fontWeight={'bold'} cursor={'pointer'} pl={'20px'}>
-              <ListIcon as={MdSsidChart} boxSize={5} />
+              </ListItem>
+              <ListItem fontWeight={"bold"} cursor={"pointer"} pl={"20px"}>
+                <ListIcon as={MdSsidChart} boxSize={5} />
                 Insight
-              </ListItem> 
-                  
-              <ListItem fontWeight={'bold'} cursor={'pointer'} pl={'20px'}>
+              </ListItem>
+
+              <ListItem fontWeight={"bold"} cursor={"pointer"} pl={"20px"}>
                 <ListIcon as={MdSettingsSuggest} boxSize={5} />
                 Setting
               </ListItem>
-              <ListItem fontWeight={'bold'} cursor={'pointer'} pl={'20px'}>
+              <ListItem fontWeight={"bold"} cursor={"pointer"} pl={"20px"}>
                 <ListIcon as={MdHelp} boxSize={5} />
                 Support and Help
               </ListItem>
-              <ListItem fontWeight={'bold'} cursor={'pointer'} pl={'20px'}>
+              <ListItem fontWeight={"bold"} cursor={"pointer"} pl={"20px"}>
                 <ListIcon as={MdAnnouncement} boxSize={5} />
                 Annoucement
               </ListItem>
               <ListItem>
-                <Button colorScheme="red" p={'10px'} w={'75%'}>Logout</Button>
+                <Button colorScheme="red" p={"10px"} w={"75%"}>
+                  Logout
+                </Button>
               </ListItem>
             </List>
           </Flex>
-          </GridItem>
-          <GridItem 
-          as={'main'}
-          colSpan={'5'}
-          >
-            <Flex  borderBottom={'1px solid grey'} p={'10px'} align={'center'}>
-              <Box ml={'25px'}>
-                <img src={logo} alt="logo"/>
-              </Box>
-              <Spacer/>
-              <Flex align={'center'} gap={'10'} mr={'100px'}>
-              <IconButton variant={'ghost'} colorScheme="white" icon={<SearchIcon/>}/>
+        </GridItem>
+        <GridItem as={"main"} colSpan={"5"}>
+          <Flex borderBottom={"1px solid grey"} p={"10px"} align={"center"}>
+            <Box ml={"25px"}>
+              <img src={logo} alt="logo" />
+            </Box>
+            <Spacer />
+            <Flex align={"center"} gap={"10"} mr={"100px"}>
+              <IconButton
+                variant={"ghost"}
+                colorScheme="white"
+                icon={<SearchIcon />}
+              />
               <Menu>
-              <MenuButton>
-              <IconButton variant={'ghost'} colorScheme="white" icon={<AddIcon/>}/>
-              </MenuButton>
-              <MenuList>
-              <MenuItem>Create Attendance</MenuItem>
-              <MenuItem>Join Attendance</MenuItem>
-              </MenuList>
+                <MenuButton>
+                  <IconButton
+                    variant={"ghost"}
+                    colorScheme="white"
+                    icon={<AddIcon />}
+                  />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Create Attendance</MenuItem>
+                  <MenuItem>Join Attendance</MenuItem>
+                </MenuList>
               </Menu>
-              
+
               <Wrap>
                 <WrapItem>
-                 <Avatar name="Segun Showunmi" src="https://bit.ly/broken-link"/>
+                  <Avatar
+                    name="University of Ibadan"
+                    src="https://bit.ly/broken-link"
+                  />
                 </WrapItem>
               </Wrap>
-              </Flex>
-              
             </Flex>
-          <Outlet/>
-          </GridItem>
-          
-        </Grid>
+          </Flex>
+          <Outlet />
+        </GridItem>
+      </Grid>
     </div>
-  )
+  );
 }
