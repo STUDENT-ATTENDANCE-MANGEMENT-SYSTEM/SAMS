4011c087627213188e408d4c26231f94c2e56a70
Eniola Oladejo <enioladejo1725@gmail.com>
2023-10-24 13:28:48 +0100

    refactored route

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,157 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import "./App.css"
+// import { Home } from "./pages/Home"
+// import About from "./pages/About"
+// import Price from "./pages/Price"
+// import Contact from "./pages/Contact"
+// import Login from "./pages/Login"
+// import RootLayouts from "./layouts/RootLayouts"
+// import NotFound from "./pages/NotFound"
+
+// import Student, { studentRegister } from "./pages/signin/Student"
+// import Lecturer, { lecturerRegister } from "./pages/signin/Lecturer"
+// import RegisterInstitution, {
+//   institutionRegister,
+// } from "./pages/Register_Institution"
+// import InstitutionMain from "./pages/institution/InstitutionMain"
+// import StudentLayout from "./layouts/StudentLayout"
+// import RegisterLayout from "./layouts/RegisterLayout"
+// import Signin from "./pages/Signin"
+// import LecturerLayout from "./layouts/LecturerLayout"
+// import LecturerMain from "./pages/lecturer/LecturerMain"
+// import StudentMain from "./pages/student/StudentMain"
+// import LecturerHome from "./pages/lecturer/LecturerHome"
+// import InstitutionLayout from "./layouts/InstitutionLayout"
+// import Construction from "./pages/Construction"
+// import InstitutionHome from "./pages/institution/InstitutionHome"
+// // import Navbar from "./component/Navbar"
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' element={<RootLayouts />}>
+//           <Route index element={<Home />} />
+//           <Route path='about' element={<About />} />
+//           <Route path='price' element={<Price />} />
+//           <Route path='contact' element={<Contact />} />
+//           <Route path='login' element={<Login />} />
+//           <Route
+//             path='register-institution'
+//             element={<RegisterInstitution />}
+//             // action={institutionRegister}
+//           />
+//         </Route>
+
+//         <Route path='signin' element={<RegisterLayout />}>
+//           <Route index element={<Signin />} />
+//           <Route
+//             path='student'
+//             element={<Student />}
+//             action={studentRegister}
+//           />
+//           <Route
+//             path='lecturer'
+//             element={<Lecturer />}
+//             action={lecturerRegister}
+//           />
+//         </Route>
+//         <Route path='lecturer' element={<LecturerLayout />}>
+//           <Route index element={<LecturerMain />} />
+//           <Route path='home' element={<LecturerHome />} />
+//         </Route>
+//         <Route path='student' element={<StudentLayout />}>
+//           <Route index element={<StudentMain />} />
+//         </Route>
+//         <Route path='institution' element={<InstitutionLayout />}>
+//           <Route index element={<InstitutionMain />} />
+//           <Route path='home' element={<InstitutionHome />} />
+//         </Route>
+//         <Route path='*' element={<NotFound />} />
+//         <Route path='construction' element={<Construction />} />
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-} from "react-router-dom";
-import "./App.css";
-import { Home } from "./pages/Home";
-import About from "./pages/About";
-import Price from "./pages/Price";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import RootLayouts from "./layouts/RootLayouts";
-import NotFound from "./pages/NotFound";
+  Routes,
+} from "react-router-dom"
+import "./App.css"
+import { Home } from "./pages/Home"
+import About from "./pages/About"
+import Price from "./pages/Price"
+import Contact from "./pages/Contact"
+import Login from "./pages/Login"
+import RootLayouts from "./layouts/RootLayouts"
+import NotFound from "./pages/NotFound"
 
-import Student, { studentRegister } from "./pages/signin/Student";
-import Lecturer, { lecturerRegister } from "./pages/signin/Lecturer";
+import Student, { studentRegister } from "./pages/signin/Student"
+import Lecturer, { lecturerRegister } from "./pages/signin/Lecturer"
 import Register_Institution, {
   institutionRegister,
-} from "./pages/Register_Institution";
-import InstitutionMain from "./pages/institution/InstitutionMain";
-import StudentLayout from "./layouts/StudentLayout";
-import RegisterLayout from "./layouts/RegisterLayout";
-import Signin from "./pages/Signin";
-import LecturerLayout from "./layouts/LecturerLayout";
-import LecturerMain from "./pages/lecturer/LecturerMain";
-import StudentMain from "./pages/student/StudentMain";
-import LecturerHome from "./pages/lecturer/LecturerHome";
-import InstitutionLayout from "./layouts/InstitutionLayout";
-import Construction from "./pages/Construction";
-import InstitutionHome from "./pages/institution/InstitutionHome";
+} from "./pages/Register_Institution"
+import InstitutionMain from "./pages/institution/InstitutionMain"
+import StudentLayout from "./layouts/StudentLayout"
+import RegisterLayout from "./layouts/RegisterLayout"
+import Signin from "./pages/Signin"
+import LecturerLayout from "./layouts/LecturerLayout"
+import LecturerMain from "./pages/lecturer/LecturerMain"
+import StudentMain from "./pages/student/StudentMain"
+import LecturerHome from "./pages/lecturer/LecturerHome"
+import InstitutionLayout from "./layouts/InstitutionLayout"
+import Construction from "./pages/Construction"
+import InstitutionHome from "./pages/institution/InstitutionHome"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayouts />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="price" element={<Price />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="login" element={<Login />} />
-      <Route
-        path="register-institution"
-        element={<Register_Institution />}
-        action={institutionRegister}
-      />
-      <Route path="signin" element={<RegisterLayout />}>
-        <Route index element={<Signin />} />
-        <Route path="student" element={<Student />} action={studentRegister} />
+    <>
+      <Route path='/' element={<RootLayouts />}>
+        <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='price' element={<Price />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='login' element={<Login />} />
         <Route
-          path="lecturer"
+          path='register-institution'
+          element={<Register_Institution />}
+          action={institutionRegister}
+        />
+      </Route>
+      <Route path='signin' element={<RegisterLayout />}>
+        <Route index element={<Signin />} />
+        <Route path='student' element={<Student />} action={studentRegister} />
+        <Route
+          path='lecturer'
           element={<Lecturer />}
           action={lecturerRegister}
         />
       </Route>
-      <Route path="lecturer" element={<LecturerLayout />}>
+      <Route path='lecturer' element={<LecturerLayout />}>
         <Route index element={<LecturerMain />} />
-        <Route path="home" element={<LecturerHome />} />
+        <Route path='home' element={<LecturerHome />} />
       </Route>
-      <Route path="student" element={<StudentLayout />}>
+      <Route path='student' element={<StudentLayout />}>
         <Route index element={<StudentMain />} />
       </Route>
-      <Route path="institution" element={<InstitutionLayout />}>
+      <Route path='institution' element={<InstitutionLayout />}>
         <Route index element={<InstitutionMain />} />
-        <Route path="home" element={<InstitutionHome />} />
+        <Route path='home' element={<InstitutionHome />} />
       </Route>
-      <Route path="*" element={<NotFound />} />
-      <Route path="construction" element={<Construction />} />
-    </Route>
+      <Route path='*' element={<NotFound />} />
+      <Route path='construction' element={<Construction />} />
+    </>
   )
-);
+)
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />
 }
 
-export default App;
+export default App
--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -30,23 +30,23 @@ import {
   Tabs,
   Text,
   useDisclosure,
-} from "@chakra-ui/react";
-import image from "../images/25.png";
-import { useEffect, useState } from "react";
-import { Link, NavLink, Outlet } from "react-router-dom";
-import tag from "../images/price-tag.png";
-import wallet from "../images/wallet.png";
-import diamond from "../images/diamond.png";
-import anchor from "../images/anchor.png";
-import target from "../images/target.png";
-import rocket from "../images/rocket.png";
-import visa from "../images/visa.svg";
-import mastercard from "../images/mastercard.svg";
-import paystack from "../images/paystack.svg";
-import chimoney from "../images/chimoney.png";
-import googlepay from "../images/googlepay.svg";
-import Navbar from "../component/Navbar";
-import Footer from "../component/Footer";
+} from "@chakra-ui/react"
+import image from "../images/25.png"
+import { useEffect, useState } from "react"
+import { Link, NavLink, Outlet } from "react-router-dom"
+import tag from "../images/price-tag.png"
+import wallet from "../images/wallet.png"
+import diamond from "../images/diamond.png"
+import anchor from "../images/anchor.png"
+import target from "../images/target.png"
+import rocket from "../images/rocket.png"
+import visa from "../images/visa.svg"
+import mastercard from "../images/mastercard.svg"
+import paystack from "../images/paystack.svg"
+import chimoney from "../images/chimoney.png"
+import googlepay from "../images/googlepay.svg"
+// import Navbar from "../component/Navbar"
+import Footer from "../component/Footer"
 
 import {
   CalendarIcon,
@@ -57,42 +57,46 @@ import {
   StarIcon,
   TimeIcon,
   WarningIcon,
-} from "@chakra-ui/icons";
+} from "@chakra-ui/icons"
 export default function Price() {
   useEffect(() => {
-    document.body.classList.add("bg-color");
-  }, []);
+    document.body.classList.add("bg-color")
+  }, [])
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [amount, setAmount] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [amount, setAmount] = useState("")
 
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Flex
         flexDir={"row"}
         align={"center"}
         justifyContent={"center"}
-        mt={"100px"}>
+        mt={"100px"}
+      >
         <Heading fontFamily={"mono"} px={"10px"}>
           Pricing Plan
         </Heading>
         <Box w={"50px"}>
-          <img src={tag} alt="price-tag" />
+          <img src={tag} alt='price-tag' />
         </Box>
       </Flex>
       <SimpleGrid
         columns={{ base: "1", md: "1", lg: "2", xl: "2" }}
-        mt={{ base: "0px", lg: "40px", xl: "50px" }}>
+        mt={{ base: "0px", lg: "40px", xl: "50px" }}
+      >
         <Flex
           flexDir={"column"}
           align={{ base: "center", lg: "left", xl: "left" }}
           ml={{ base: "20px", md: "40px", lg: "50px", xl: "250px" }}
-          pt={"80px"}>
+          pt={"80px"}
+        >
           <Heading
             fontSize={{ base: "40px", xl: "5xl" }}
             px={"10px"}
-            pb={"10px"}>
+            pb={"10px"}
+          >
             Choose the plan <br /> that best fits your <br /> institution's
             needs
           </Heading>
@@ -103,7 +107,7 @@ export default function Price() {
         <Box ml={"auto"} mr={"auto"}>
           <img
             src={wallet}
-            alt="price-tag"
+            alt='price-tag'
             style={{
               height: "auto",
               width: "100%",
@@ -113,7 +117,7 @@ export default function Price() {
           />
         </Box>
       </SimpleGrid>
-      <Tabs colorScheme="pink" mt={"40px"} p={"20px"} variant={"enclosed"}>
+      <Tabs colorScheme='pink' mt={"40px"} p={"20px"} variant={"enclosed"}>
         <TabList ml={"auto"} mr={"auto"} justifyContent={"center"}>
           <Tab _selected={{ color: "white", bg: "red.500" }} mr={"30px"}>
             Month
@@ -126,17 +130,19 @@ export default function Price() {
               flexDir={{ base: "column", md: "colum", lg: "row", xl: "row" }}
               gap={10}
               mx={"35px"}
-              my={"30px"}>
+              my={"30px"}
+            >
               <Flex align={"center"} flexDir={"column"}>
                 <Box onClick={onOpen}>
                   <Card
                     onClick={() =>
                       setAmount("Small Sized Institution :" + " NGN 3,000")
-                    }>
+                    }
+                  >
                     <CardHeader borderBottom={"1px solid grey"}>
                       <Flex flexDir={"column"} align={"center"}>
                         <Box w={"60px"} py={"20px"}>
-                          <img src={anchor} alt="basic" />
+                          <img src={anchor} alt='basic' />
                         </Box>
 
                         <Heading fontFamily={"mono"} pb={"20px"}>
@@ -184,7 +190,7 @@ export default function Price() {
                   </Card>
                 </Box>
                 <Link to={"/institution/home"}>
-                  <Button my={"20px"} color={"white"} colorScheme="red">
+                  <Button my={"20px"} color={"white"} colorScheme='red'>
                     Try Free Trial
                   </Button>
                 </Link>
@@ -194,11 +200,12 @@ export default function Price() {
                   <Card
                     onClick={() =>
                       setAmount("Medium Sized Institution :" + " NGN 5,000")
-                    }>
+                    }
+                  >
                     <CardHeader borderBottom={"1px solid grey"}>
                       <Flex flexDir={"column"} align={"center"}>
                         <Box w={"60px"} py={"20px"}>
-                          <img src={target} alt="basic" />
+                          <img src={target} alt='basic' />
                         </Box>
 
                         <Heading fontFamily={"mono"} pb={"20px"}>
@@ -247,7 +254,7 @@ export default function Price() {
                   </Card>
                 </Box>
                 <Link to={"/institution/home"}>
-                  <Button my={"20px"} color={"white"} colorScheme="red">
+                  <Button my={"20px"} color={"white"} colorScheme='red'>
                     Try Free Trial
                   </Button>
                 </Link>
@@ -257,11 +264,12 @@ export default function Price() {
                   <Card
                     onClick={() =>
                       setAmount("Large Sized Institution :" + " NGN 7,000")
-                    }>
+                    }
+                  >
                     <CardHeader borderBottom={"1px solid grey"}>
                       <Flex flexDir={"column"} align={"center"}>
                         <Box w={"60px"} py={"20px"}>
-                          <img src={rocket} alt="basic" />
+                          <img src={rocket} alt='basic' />
                         </Box>
 
                         <Heading fontFamily={"mono"} pb={"20px"}>
@@ -310,7 +318,7 @@ export default function Price() {
                   </Card>
                 </Box>
                 <Link to={"/institution/home"}>
-                  <Button my={"20px"} color={"white"} colorScheme="red">
+                  <Button my={"20px"} color={"white"} colorScheme='red'>
                     Try Free Trial
                   </Button>
                 </Link>
@@ -320,11 +328,12 @@ export default function Price() {
                   <Card
                     onClick={() =>
                       setAmount("Very Large Sized Institution :" + " NGN 9,000")
-                    }>
+                    }
+                  >
                     <CardHeader borderBottom={"1px solid grey"}>
                       <Flex flexDir={"column"} align={"center"}>
                         <Box w={"60px"} py={"20px"}>
-                          <img src={diamond} alt="basic" />
+                          <img src={diamond} alt='basic' />
                         </Box>
 
                         <Heading fontFamily={"mono"} pb={"20px"}>
@@ -373,7 +382,7 @@ export default function Price() {
                   </Card>
                 </Box>
                 <Link to={"/institution/home"}>
-                  <Button my={"20px"} color={"white"} colorScheme="red">
+                  <Button my={"20px"} color={"white"} colorScheme='red'>
                     Try Free Trial
                   </Button>
                 </Link>
@@ -385,19 +394,24 @@ export default function Price() {
               flexDir={{ base: "column", md: "column", lg: "row", xl: "row" }}
               gap={10}
               mx={"35px"}
-              my={"30px"}>
+              my={"30px"}
+            >
               <Flex align={"center"} flexDir={"column"}>
                 <Box onClick={onOpen}>
                   <Card
                     onClick={() =>
                       setAmount("Small Sized Institution : " + " NGN 30,000")
-                    }>
+                    }
+                  >
                     <CardHeader borderBottom={"1px solid grey"}>
                       <Flex flexDir={"column"} align={"center"}>
                         <Box w={"60px"} py={"20px"}>
-                          <img src={anchor} alt="basic" />
+                          <img src={anchor} alt='basic' />
                         </Box>
 
+                        {/* <Heading fontFamily={"mono"} pb={"20px"}>
+                          NGN 30,000/yr
+                        </Heading> */}
                         <Heading fontFamily={"mono"} pb={"20px"}>
                           NGN 30,000/yr
                         </Heading>
@@ -443,7 +457,7 @@ export default function Price() {
                   </Card>
                 </Box>
                 <Link to={"/institution/home"}>
-                  <Button my={"20px"} color={"white"} colorScheme="red">
+                  <Button my={"20px"} color={"white"} colorScheme='red'>
                     Try Free Trial
                   </Button>
                 </Link>
@@ -453,11 +467,12 @@ export default function Price() {
                   <Card
                     onClick={() =>
                       setAmount("Medium Sized Institution :" + " NGN 50,000")
-                    }>
+                    }
+                  >
                     <CardHeader borderBottom={"1px solid grey"}>
                       <Flex flexDir={"column"} align={"center"}>
                         <Box w={"60px"} py={"20px"}>
-                          <img src={target} alt="basic" />
+                          <img src={target} alt='basic' />
                         </Box>
 
                         <Heading fontFamily={"mono"} pb={"20px"}>
@@ -506,7 +521,7 @@ export default function Price() {
                   </Card>
                 </Box>
                 <Link to={"/institution/home"}>
-                  <Button my={"20px"} color={"white"} colorScheme="red">
+                  <Button my={"20px"} color={"white"} colorScheme='red'>
                     Try Free Trial
                   </Button>
                 </Link>
@@ -516,11 +531,12 @@ export default function Price() {
                   <Card
                     onClick={() =>
                       setAmount("Large Sized Institution :" + " NGN 70,000")
-                    }>
+                    }
+                  >
                     <CardHeader borderBottom={"1px solid grey"}>
                       <Flex flexDir={"column"} align={"center"}>
                         <Box w={"60px"} py={"20px"}>
-                          <img src={rocket} alt="basic" />
+                          <img src={rocket} alt='basic' />
                         </Box>
 
                         <Heading fontFamily={"mono"} pb={"20px"}>
@@ -569,7 +585,7 @@ export default function Price() {
                   </Card>
                 </Box>
                 <Link to={"/institution/home"}>
-                  <Button my={"20px"} color={"white"} colorScheme="red">
+                  <Button my={"20px"} color={"white"} colorScheme='red'>
                     Try Free Trial
                   </Button>
                 </Link>
@@ -581,11 +597,12 @@ export default function Price() {
                       setAmount(
                         "Very Large Sized Institution :" + " NGN 90,000"
                       )
-                    }>
+                    }
+                  >
                     <CardHeader borderBottom={"1px solid grey"}>
                       <Flex flexDir={"column"} align={"center"}>
                         <Box w={"60px"} py={"20px"}>
-                          <img src={diamond} alt="basic" />
+                          <img src={diamond} alt='basic' />
                         </Box>
 
                         <Heading fontFamily={"mono"} pb={"20px"}>
@@ -634,7 +651,7 @@ export default function Price() {
                   </Card>
                 </Box>
                 <Link to={"/institution/home"}>
-                  <Button my={"20px"} color={"white"} colorScheme="red">
+                  <Button my={"20px"} color={"white"} colorScheme='red'>
                     Try Free Trial
                   </Button>
                 </Link>
@@ -662,28 +679,28 @@ export default function Price() {
             <Flex flexDir={"row"} align={"center"} gap={6} pr={"30px"}>
               <Link to={"/"}>
                 <Box w={"50px"}>
-                  <img src={visa} alt="visa" />
+                  <img src={visa} alt='visa' />
                 </Box>
               </Link>
 
               <Link to={"/"}>
                 <Box w={"50px"}>
-                  <img src={mastercard} alt="visa" />
+                  <img src={mastercard} alt='visa' />
                 </Box>
               </Link>
               <Link to={"/paystack-check-out"}>
                 <Box w={"70px"}>
-                  <img src={paystack} alt="visa" />
+                  <img src={paystack} alt='visa' />
                 </Box>
               </Link>
               <Link>
                 <Box w={"50px"}>
-                  <img src={chimoney} alt="visa" />
+                  <img src={chimoney} alt='visa' />
                 </Box>
               </Link>
               <Link to={"/"}>
                 <Box w={"30px"}>
-                  <img src={googlepay} alt="visa" />
+                  <img src={googlepay} alt='visa' />
                 </Box>
               </Link>
             </Flex>
@@ -693,5 +710,5 @@ export default function Price() {
       <Footer />
       <Outlet />
     </div>
-  );
+  )
 }
