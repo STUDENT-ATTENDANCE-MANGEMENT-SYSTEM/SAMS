d46213b659716cc2e2a064dbeacb9f6c2db30702
Eniola Oladejo <enioladejo1725@gmail.com>
2023-10-29 14:56:14 +0100

    fixed bug

--- src/pages/lecturer/LecturerHome.jsx
+++ src/pages/lecturer/LecturerHome.jsx
@@ -14,30 +14,31 @@ import {
   MenuList,
   Spacer,
   Icon,
-} from "@chakra-ui/react";
-import { v4 as uuidv4 } from "uuid";
-import { useEffect, useState } from "react";
-import copy from "copy-to-clipboard";
-import { NavLink, Outlet } from "react-router-dom";
-import { color } from "framer-motion";
+} from "@chakra-ui/react"
+import { v4 as uuidv4 } from "uuid"
+import { useEffect, useState } from "react"
+import copy from "copy-to-clipboard"
+import { NavLink, Outlet } from "react-router-dom"
+import { color } from "framer-motion"
 import {
   MdContentCopy,
   MdMoreVert,
   MdLockOutline,
   MdEdit,
   MdCalculate,
-} from "react-icons/md";
+  MdLockOpen,
+} from "react-icons/md"
 const data = [
   { courseCode: "TME 211", title: "INTRODUCTION TO ENGINEERING DRAWING" },
   { courseCode: "TME 231", title: "INTRODUCTION TO ENGINEERING PRINCIPLES" },
   { courseCode: "MAT 224", title: "INTRODUCTION TO ENGINEERING MATHEMATICS" },
   { courseCode: "GNS 210", title: "INTRODUCTION TO ENTREPRENEURSHIP" },
-];
+]
 
 export default function LecturerHome() {
   useEffect(() => {
-    document.body.classList.add("bg-color");
-  }, []);
+    document.body.classList.add("bg-color")
+  }, [])
   const [lock, setLock] = useState(Array(data.length).fill(true))
   const handleOpen = (index) => {
     setLock((prevLock) => {
@@ -45,19 +46,20 @@ export default function LecturerHome() {
       updatedLock[index] = !lock[index]
       return updatedLock
     })
-}
+  }
   return (
     <div>
       <SimpleGrid
         columns={{ base: "2", lg: "3", xl: "3" }}
         mx={"20px"}
         gap={"10"}
-        mt={"30px"}>
-        {data.map((item) => {
-          const { courseCode, title } = item;
-          const [code, setCode] = useState();
+        mt={"30px"}
+      >
+        {data.map((item, index) => {
+          const { courseCode, title } = item
+          const [code, setCode] = useState()
           return (
-            <Card>
+            <Card key={index}>
               <CardHeader>
                 <Flex>
                   <Text fontSize={"1.6rem"} pl={"10px"} mt={"15px"}>
@@ -86,15 +88,16 @@ export default function LecturerHome() {
               <CardFooter>
                 <Spacer />
                 <IconButton
+                  onClick={() => handleOpen(index)}
                   variant={"ghost"}
-                  icon={<MdLockOutline />}
+                  icon={lock[index] ? <MdLockOutline /> : <MdLockOpen />}
                   size={"lg"}
                 />
                 <IconButton
                   onClick={() => {
-                    setCode(courseCode + uuidv4());
-                    copy(code);
-                    alert(`You have copied ${courseCode} attendance code`);
+                    setCode(courseCode + uuidv4())
+                    copy(code)
+                    alert(`You have copied ${courseCode} attendance code`)
                   }}
                   variant={"ghost"}
                   size={"lg"}
@@ -102,9 +105,9 @@ export default function LecturerHome() {
                 />
               </CardFooter>
             </Card>
-          );
+          )
         })}
       </SimpleGrid>
     </div>
-  );
+  )
 }
