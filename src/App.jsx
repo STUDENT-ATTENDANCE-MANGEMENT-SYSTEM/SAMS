d237779f2d99fa30590329b09259e2635d25c309
Eniola Oladejo <enioladejo1725@gmail.com>
2023-12-11 02:41:09 +0100

    worked on lecturer's form

--- src/App.jsx
+++ src/App.jsx
@@ -1,81 +1,3 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom"
-// import "./App.css"
-// import { Home } from "./pages/Home"
-// import About from "./pages/About"
-// import Price from "./pages/Price"
-// import Contact from "./pages/Contact"
-// import Login from "./pages/Login"
-// import RootLayouts from "./layouts/RootLayouts"
-// import NotFound from "./pages/NotFound"
-
-// import Student, { studentRegister } from "./pages/signin/Student"
-// import Lecturer, { lecturerRegister } from "./pages/signin/Lecturer"
-// import RegisterInstitution, {
-//   institutionRegister,
-// } from "./pages/Register_Institution"
-// import InstitutionMain from "./pages/institution/InstitutionMain"
-// import StudentLayout from "./layouts/StudentLayout"
-// import RegisterLayout from "./layouts/RegisterLayout"
-// import Signin from "./pages/Signin"
-// import LecturerLayout from "./layouts/LecturerLayout"
-// import LecturerMain from "./pages/lecturer/LecturerMain"
-// import StudentMain from "./pages/student/StudentMain"
-// import LecturerHome from "./pages/lecturer/LecturerHome"
-// import InstitutionLayout from "./layouts/InstitutionLayout"
-// import Construction from "./pages/Construction"
-// import InstitutionHome from "./pages/institution/InstitutionHome"
-// // import Navbar from "./component/Navbar"
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path='/' element={<RootLayouts />}>
-//           <Route index element={<Home />} />
-//           <Route path='about' element={<About />} />
-//           <Route path='price' element={<Price />} />
-//           <Route path='contact' element={<Contact />} />
-//           <Route path='login' element={<Login />} />
-//           <Route
-//             path='register-institution'
-//             element={<RegisterInstitution />}
-//             // action={institutionRegister}
-//           />
-//         </Route>
-
-//         <Route path='signin' element={<RegisterLayout />}>
-//           <Route index element={<Signin />} />
-//           <Route
-//             path='student'
-//             element={<Student />}
-//             action={studentRegister}
-//           />
-//           <Route
-//             path='lecturer'
-//             element={<Lecturer />}
-//             action={lecturerRegister}
-//           />
-//         </Route>
-//         <Route path='lecturer' element={<LecturerLayout />}>
-//           <Route index element={<LecturerMain />} />
-//           <Route path='home' element={<LecturerHome />} />
-//         </Route>
-//         <Route path='student' element={<StudentLayout />}>
-//           <Route index element={<StudentMain />} />
-//         </Route>
-//         <Route path='institution' element={<InstitutionLayout />}>
-//           <Route index element={<InstitutionMain />} />
-//           <Route path='home' element={<InstitutionHome />} />
-//         </Route>
-//         <Route path='*' element={<NotFound />} />
-//         <Route path='construction' element={<Construction />} />
-//       </Routes>
-//     </BrowserRouter>
-//   )
-// }
-
-// export default App
-
 import {
   Route,
   createBrowserRouter,
@@ -83,6 +5,8 @@ import {
   RouterProvider,
   Routes,
 } from "react-router-dom"
+import { useState, createContext } from "react"
+
 import "./App.css"
 import { Home } from "./pages/Home"
 import About from "./pages/About"
@@ -93,7 +17,7 @@ import RootLayouts from "./layouts/RootLayouts"
 import NotFound from "./pages/NotFound"
 
 import Student, { studentRegister } from "./pages/signin/Student"
-import Lecturer, { lecturerRegister } from "./pages/signin/Lecturer"
+import Lecturer from "./pages/signin/Lecturer"
 import Register_Institution, {
   institutionRegister,
 } from "./pages/Register_Institution"
@@ -108,6 +32,9 @@ import LecturerHome from "./pages/lecturer/LecturerHome"
 import InstitutionLayout from "./layouts/InstitutionLayout"
 import Construction from "./pages/Construction"
 import InstitutionHome from "./pages/institution/InstitutionHome"
+import ProtectedRoute from "./layouts/ProtectedRoute"
+
+export const appContext = createContext()
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -126,14 +53,25 @@ const router = createBrowserRouter(
       </Route>
       <Route path='signin' element={<RegisterLayout />}>
         <Route index element={<Signin />} />
-        <Route path='student' element={<Student />} action={studentRegister} />
+        <Route
+          path='student'
+          element={<Student />}
+          // action={studentRegister}
+        />
         <Route
           path='lecturer'
           element={<Lecturer />}
-          action={lecturerRegister}
+          // action={lecturerRegister}
         />
       </Route>
-      <Route path='lecturer' element={<LecturerLayout />}>
+      <Route
+        path='lecturer'
+        element={
+          <ProtectedRoute>
+            <LecturerLayout />
+          </ProtectedRoute>
+        }
+      >
         <Route index element={<LecturerMain />} />
         <Route path='home' element={<LecturerHome />} />
       </Route>
@@ -151,7 +89,13 @@ const router = createBrowserRouter(
 )
 
 function App() {
-  return <RouterProvider router={router} />
+  const [student, setStudent] = useState(null)
+  const [lecturer, setLecturer] = useState(null)
+  return (
+    <appContext.Provider value={{ student, setStudent, lecturer, setLecturer }}>
+      <RouterProvider router={router} />
+    </appContext.Provider>
+  )
 }
 
 export default App
